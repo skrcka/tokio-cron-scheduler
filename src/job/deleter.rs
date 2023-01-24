35bd62e6b0a9415807654f58096a7a745038b476
skrcka <krcek.adam@gmail.com>
2023-01-24 23:44:40 +0100

    remove fix

--- src/job/deleter.rs
+++ src/job/deleter.rs
@@ -56,41 +56,42 @@ impl JobDeleter {
         })
     }
 
-    pub fn remove(context: &Context, job_id: &Uuid) -> Result<(), JobSchedulerError> {
+    pub async fn remove(context: &Context, job_id: &Uuid) -> Result<(), JobSchedulerError> {
         let delete = context.job_delete_tx.clone();
         let mut deleted = context.job_deleted_tx.subscribe();
         let (tx, rx) = std::sync::mpsc::channel();
 
         let job_id = *job_id;
         tokio::spawn(async move {
-            tokio::spawn(async move {
-                if let Err(e) = delete.send(job_id) {
-                    error!("Error sending delete id {:?}", e);
-                }
-            });
-            while let Ok(deleted) = deleted.recv().await {
-                let ret = match deleted {
-                    Ok(uuid) => {
-                        if uuid == job_id {
-                            Ok(())
-                        } else {
-                            continue;
-                        }
-                    }
-                    Err((e, Some(uuid))) => {
-                        if uuid == job_id {
-                            Err(e)
-                        } else {
-                            continue;
-                        }
-                    }
-                    _ => continue,
-                };
-                if let Err(e) = tx.send(ret) {
-                    error!("Error sending removal result {:?}", e);
-                }
+            if let Err(e) = delete.send(job_id) {
+                error!("Error sending delete id {:?}", e);
             }
         });
+        while let Ok(deleted) = deleted.recv().await {
+            let ret = match deleted {
+                Ok(uuid) => {
+                    if uuid == job_id {
+                        Ok(())
+                    } else {
+                        continue;
+                    }
+                }
+                Err((e, Some(uuid))) => {
+                    if uuid == job_id {
+                        Err(e)
+                    } else {
+                        continue;
+                    }
+                }
+                _ => continue,
+            };
+            if let Err(e) = tx.send(ret) {
+                error!("Error sending removal result {:?}", e);
+            }
+            else {
+                break;
+            }
+        }
 
         let ret = rx.recv();
         match ret {
